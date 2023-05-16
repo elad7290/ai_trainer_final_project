20950bde0123fa5cd4c596c47ee112bacf391c47
elad7290 <elad7290@gmail.com>
2023-05-16 15:15:00 +0300

    comments

--- lib/models/user_table.dart
+++ lib/models/user_table.dart
@@ -1,21 +1,28 @@
-/*
-class MyUser{
-  final String email;
-  final String password;
-
-  MyUser({
-    required this.email,
-    required this.password
-  });
-
-  Map<String, dynamic> toJson() => {
-    'email': email,
-    'password': password,
-  };
-
-  static MyUser fromJson(Map<String, dynamic> json) =>
-      MyUser(
-          email: json['email'],
-          password: json['password']
-      );
-}*/
+//
+// class MyUser{
+//   final String name;
+//   final DateTime birthDate;
+//   final int level;
+//   final double weight;
+//   final double height;
+//   //final map<int,double>
+//   //final Imag pic
+//
+//
+//
+//   MyUser({
+//     required this.email,
+//     required this.password
+//   });
+//
+//   Map<String, dynamic> toJson() => {
+//     'email': email,
+//     'password': password,
+//   };
+//
+//   static MyUser fromJson(Map<String, dynamic> json) =>
+//       MyUser(
+//           email: json['email'],
+//           password: json['password']
+//       );
+//
