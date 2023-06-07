905ccc572c411aa7e759f66ea1fca8c2d53a0f2e
Or-Memia <ormem25@gmail.com>
2023-06-07 19:44:07 +0300

    I added index.html file. I integrated the "Push ups" model in the application.

--- lib/views/pages/camera_page.dart
+++ lib/views/pages/camera_page.dart
@@ -1,8 +1,10 @@
 import 'package:ai_trainer/views/widgets/flip_camera_button.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-
+import 'dart:convert';
 import '../widgets/excercise_count.dart';
+import 'package:teachable/teachable.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -11,7 +13,7 @@ class CameraScreen extends StatefulWidget {
   State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _CameraScreenState extends State<CameraScreen> {
+class _CameraScreenState extends State<CameraScreen> with WidgetsBindingObserver {
   late List<CameraDescription> cameras;
   late CameraController camera_controller;
   bool isCameraInitialized = false;
@@ -20,11 +22,47 @@ class _CameraScreenState extends State<CameraScreen> {
   int repetitions = 0;
   int sets = 0;
 
+  String currentExercise = "";
+  String confidence = "";
+  bool isLoading = false; // Added loading indicator
+
+  Map<String, String> exercises = {
+    'Push Ups': 'Push Ups',
+    'Nothing': 'Nothing',
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(this);
+    initCamera();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    camera_controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      initCamera();
+    }
+  }
+
   void initCamera() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Permission.camera.request();
     cameras = await availableCameras();
     camera_controller = CameraController(
-        cameras[direction], ResolutionPreset.high,
-        enableAudio: false);
+      cameras[direction],
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
     await camera_controller.initialize().then((value) {
       if (!mounted) {
         return;
@@ -44,51 +82,79 @@ class _CameraScreenState extends State<CameraScreen> {
     });
   }
 
-  @override
-  void initState() {
-    initCamera();
-    super.initState();
+  void processResult(String result) {
+    var decodedResult = jsonDecode(result);
+    decodedResult.forEach((exercise, score) {
+      if (score > 0.5) {
+        setState(() {
+          currentExercise = exercises[exercise]!;
+          confidence = (score * 100.0).toStringAsFixed(2);
+          isLoading = false; // Set isLoading to false when the result is obtained
+        });
+        return;
+      }
+    });
   }
 
-  @override
-  void dispose() {
-    camera_controller.dispose();
-    super.dispose();
+  void startProcessing() {
+    setState(() {
+      isLoading = true; // Set isLoading to true when processing starts
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isCameraInitialized && camera_controller.value.isInitialized) {
-      return SafeArea(
-        child: Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  child: CameraPreview(camera_controller)),
-              FlipCameraButton(flip),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: Column(
-                    children: [
-                      //TODO: take sets and rep from DB
-                      ExcerciseCount("Repetitions:   " + repetitions.toString() + " / 30"),
-                      SizedBox(height: 10,),
-                      ExcerciseCount("Sets:   " + sets.toString() + " / 3"),
-                    ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pose classifier")),
+      body: Stack(
+        children: [
+          Container(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Teachable(
+                      path: "lib/assets/index.html",
+                      results: (res) {
+                        processResult(res);
+                      },
+                    ),
                   ),
                 ),
-              )
-
-            ],
+              ],
+            ),
           ),
-        ),
-      );
-    } else {
-      return const SizedBox();
-    }
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.5),
+              ),
+              child: isLoading // Conditional rendering based on isLoading
+                  ? CircularProgressIndicator() // Show loading indicator
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Exercise: $currentExercise",
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Confidence: $confidence%",
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
